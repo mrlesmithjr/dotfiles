71aa98852035d0041aa2436524f7f04cee09f281
Larry Smith Jr <mrlesmithjr@gmail.com>
2017-06-20 09:14:20 -0400

    Added install of bash-git-prompt for Linux if missing
    Signed-off-by: Larry Smith Jr <mrlesmithjr@gmail.com>

--- setup.sh
+++ setup.sh
@@ -42,3 +42,9 @@ do
   ln -s $DOTFILES_DIR/$dotfile .
   echo "Done"
 done
+
+if [[ $(uname) == "Linux" ]]; then
+  if [ ! -f $HOME/.bash-git-prompt/gitprompt.sh ]; then
+    git clone https://github.com/magicmonty/bash-git-prompt.git $HOME/.bash-git-prompt --depth=1
+  fi
+fi
