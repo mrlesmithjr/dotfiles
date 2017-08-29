c2853474d805de753f3cae47aa4c78e26ccd658c
Larry Smith Jr <mrlesmithjr@gmail.com>
2017-08-29 16:51:39 -0400

    Added logic to detect pip or pip2
    Signed-off-by: Larry Smith Jr <mrlesmithjr@gmail.com>

--- install/setup_ansible_virtualenvs.sh
+++ install/setup_ansible_virtualenvs.sh
@@ -7,13 +7,24 @@ ANSIBLE_VERSIONS=("1.9.4" "1.9.5" "1.9.6" "2.0.0.0" "2.0.0.1" "2.0.0.2" \
 ANSIBLE_CONTAINER_VERSIONS=("0.1.0" "0.2.0" "0.3.0" "0.9.0.0" "0.9.1")
 VIRTUALENV_PATH="$HOME/python-virtualenvs"
 
+# Detect pip command
+command -v pip >/dev/null 2>&1
+PIP_CHECK=$?
+command -v pip2 >/dev/null 2>&1
+PIP2_CHECK=$?
+if [ $PIP_CHECK -eq 0 ]; then
+  PIP_CMD="pip"
+  elif [ $PIP2_CHECK -eq 0 ]; then
+  PIP_CMD="pip2"
+fi
+
 # Setup Ansible Virtual Environments
 for ANSVER in "${ANSIBLE_VERSIONS[@]}"
 do
   if [ ! -d "$VIRTUALENV_PATH/ansible-$ANSVER" ]; then
     virtualenv $VIRTUALENV_PATH/ansible-$ANSVER
     source $VIRTUALENV_PATH/ansible-$ANSVER/bin/activate
-    pip install ansible==$ANSVER ansible-lint
+    $PIP_CMD install ansible==$ANSVER ansible-lint
     deactivate
   fi
 done
@@ -24,7 +35,7 @@ do
   if [ ! -d "$VIRTUALENV_PATH/ansible-container-$ANSCONTVER" ]; then
     virtualenv $VIRTUALENV_PATH/ansible-container-$ANSCONTVER
     source $VIRTUALENV_PATH/ansible-container-$ANSCONTVER/bin/activate
-    pip install ansible-container[docker]==$ANSCONTVER ansible-lint
+    $PIP_CMD install ansible-container[docker]==$ANSCONTVER ansible-lint
     deactivate
   fi
 done
