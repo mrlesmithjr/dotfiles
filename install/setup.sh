37acfd425be8abef5f9e4a573acfb6b3728777b1
Larry Smith Jr <mrlesmithjr@gmail.com>
2017-08-28 20:59:29 -0400

    Fixed detection of proper pip or pip2 to install Ansible, disabled a few packages to install
    Signed-off-by: Larry Smith Jr <mrlesmithjr@gmail.com>

--- install/setup.sh
+++ install/setup.sh
@@ -7,12 +7,20 @@ $DOTFILES_DIR/install/setup_common_folders.sh
 $DOTFILES_DIR/install/bootstrap.sh
 $DOTFILES_DIR/install/setup_ansible_virtualenvs.sh
 
-which ansible >/dev/null 2>&1
+command -v ansible >/dev/null 2>&1
 ANSIBLE_CHECK=$?
 if [ $ANSIBLE_CHECK -eq 0 ]; then
   echo "Ansible already installed"
 else
-  sudo pip install ansible ansible-lint
+  command -v pip >/dev/null 2>&1
+  PIP_CHECK=$?
+  command -v pip2 >/dev/null 2>&1
+  PIP2_CHECK=$?
+  if [ $PIP_CHECK -eq 0 ]; then
+    sudo pip install ansible ansible-lint
+    elif [ $PIP2_CHECK -eq 0 ]; then
+    sudo pip2 install ansible ansible-lint
+  fi
 fi
 
 ansible-playbook $DOTFILES_DIR/install/ansible-install-os-packages.yml -K
