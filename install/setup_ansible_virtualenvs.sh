1a6b753217c2fe292f3316db1d9972a233bd0b82
Larry Smith Jr <mrlesmithjr@gmail.com>
2018-12-04 00:30:57 -0500

    Updated Ansible versions and added timeout to prompt.

--- install/setup_ansible_virtualenvs.sh
+++ install/setup_ansible_virtualenvs.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-ANSIBLE_VERSIONS=("2.5.0" "2.5.1" "2.5.3" "2.5.4" "2.5.5" "2.5.6" "2.5.7" \
-    "2.5.8" "2.5.9" "2.5.10" "2.6.0" "2.6.1" "2.6.2" "2.6.3" "2.6.4" "2.6.5" \
-"2.6.6" "2.6.7" "2.6.8" "2.7.0" "2.7.1" "2.7.2")
+ANSIBLE_VERSIONS=("2.5.0" "2.5.1" "2.5.2" "2.5.3" "2.5.4" "2.5.5" "2.5.6" \
+    "2.5.7" "2.5.8" "2.5.9" "2.5.10" "2.5.11" "2.5.12" "2.5.13" "2.6.0" \
+    "2.6.1" "2.6.2" "2.6.3" "2.6.4" "2.6.5" "2.6.6" "2.6.7" "2.6.8" "2.6.9" \
+"2.6.10" "2.7.0" "2.7.1" "2.7.2" "2.7.3" "2.7.4")
 ANSIBLE_CONTAINER_VERSIONS=("0.1.0" "0.2.0" "0.3.0" "0.9.0.0" "0.9.1" "0.9.2")
 VIRTUALENV_PATH="$HOME/python-virtualenvs"
 
@@ -20,25 +21,32 @@ fi
 sudo $PIP_CMD install -U pip cffi pyOpenSSL
 sudo $PIP_CMD install virtualenv
 
-read -p "Install previous Ansible versions (y/n)?" choice
-case "$choice" in
-    y|Y ) echo "yes";;
-    n|N ) echo "no";;
-    * ) echo "invalid";;
-esac
-
-if [ "$choice" = "y" ]; then
-    # Setup Ansible Virtual Environments
-    for ANSVER in "${ANSIBLE_VERSIONS[@]}"
-    do
-        if [ ! -d "$VIRTUALENV_PATH/ansible-$ANSVER" ]; then
-            virtualenv $VIRTUALENV_PATH/ansible-$ANSVER
-            source $VIRTUALENV_PATH/ansible-$ANSVER/bin/activate
-            $PIP_CMD install ansible==$ANSVER ansible-lint
-            deactivate
+while :
+do
+    read -t 30 -p "Install previous Ansible versions (y/n)?" choice
+    
+    if [ $? = 0 ]; then
+        case "$choice" in
+            y|Y ) echo "yes";;
+            n|N ) echo "no";;
+            * ) echo "invalid";;
+        esac
+        if [ "$choice" = "y" ]; then
+            # Setup Ansible Virtual Environments
+            for ANSVER in "${ANSIBLE_VERSIONS[@]}"
+            do
+                if [ ! -d "$VIRTUALENV_PATH/ansible-$ANSVER" ]; then
+                    virtualenv $VIRTUALENV_PATH/ansible-$ANSVER
+                    source $VIRTUALENV_PATH/ansible-$ANSVER/bin/activate
+                    $PIP_CMD install ansible==$ANSVER ansible-lint
+                    deactivate
+                fi
+            done
         fi
-    done
-fi
+    else
+        break
+    fi
+done
 
 # Setup Ansible Container Virtual Environments
 # for ANSCONTVER in "${ANSIBLE_CONTAINER_VERSIONS[@]}"
