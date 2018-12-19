2afd26e8646e56fe939f16471fe2ecb6b034dcbf
Larry Smith Jr <mrlesmithjr@gmail.com>
2018-12-19 13:44:33 -0500

    Fixing sudo issues

--- install/setup.sh
+++ install/setup.sh
@@ -336,6 +336,6 @@ else
     fi
 fi
 
-ansible-playbook "$DOTFILES_DIR"/install/ansible-install-os-packages.yml -K
+ansible-playbook "$DOTFILES_DIR"/install/ansible-install-os-packages.yml
 ansible-playbook "$DOTFILES_DIR"/install/macos_defaults.yml
 
