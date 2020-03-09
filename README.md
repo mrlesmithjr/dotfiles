75de748fd697725953369a86c5140785e37fb839
Larry Smith Jr <mrlesmithjr@gmail.com>
2020-03-09 01:59:49 -0400

    Updated: repo info, etc.

--- README.md
+++ README.md
@@ -3,29 +3,25 @@
 
 **Table of Contents** _generated with [DocToc](https://github.com/thlorenz/doctoc)_
 
-- [REPO INFO](#repo-info)
+- [Dotfiles](#dotfiles)
   - [Usage](#usage)
     - [Clone down to your `$HOME` folder](#clone-down-to-your-home-folder)
-      - [Setup **EVERYTHING**](#setup-everything)
+    - [Setup](#setup)
   - [Various Setting Info](#various-setting-info)
-    - [`.bashrc` and `.bash_profile`](#bashrc-and-bash_profile)
-    - [`.gitconfig`](#gitconfig)
-    - [`.vimrc` and `.vim`](#vimrc-and-vim)
+    - [.bashrc and .bash_profile](#bashrc-and-bash_profile)
+    - [.gitconfig](#gitconfig)
+    - [.vimrc and .vim](#vimrc-and-vim)
   - [Some Example Plugins/bundles and etc](#some-example-pluginsbundles-and-etc)
   - [Visual Studio Code Extensions](#visual-studio-code-extensions)
-  - [Manipulating packages to install](#manipulating-packages-to-install)
   - [License](#license)
   - [Author Information](#author-information)
 
 <!-- END doctoc generated TOC please keep comment here to allow auto update -->
 
-# REPO INFO
+# Dotfiles
 
-This repo is for a collection of my `dotfiles` which I use. And also installs
-numerous applications which I use for development, automation, etc.
-
-My goal will be to make as many of these as portable between `Linux` distros
-and `MacOS`.
+A collection of my `.dotfiles` including a [setup](setup.sh) script. This script
+will setup all the things.
 
 ## Usage
 
@@ -36,127 +32,55 @@ cd ~
 git clone https://github.com/mrlesmithjr/dotfiles .dotfiles --recursive
 ```
 
-#### Setup **EVERYTHING**
+### Setup
 
 The following will be setup using this method:
 
 - dotfiles
 - Python
 - Brew
-- Brew packages
-- Brew cask packages
-- Ansible
-- Ansible Virtual Environments
 
 ```bash
-cd ~/.dotfiles/install
+cd ~/.dotfiles
 ./setup.sh
 ```
 
 ## Various Setting Info
 
-### `.bashrc` and `.bash_profile`
+### .bashrc and .bash_profile
 
 To make these portable between `Linux` and `MacOS` we need to use both. The
 reason is that for `MacOS` the default is to use `.bash_profile` and ignore
 `.bashrc` whereas on `Linux` `.bash_profile` is only used for interactive
 logins (ssh, terminal, and etc.) and ignored from GUI based terminal sessions.
-So to make these portable the only thing added to `.bash_profile` is below:
-
-```bash
-# .bash_profile
-if [ -f ~/.bashrc ]; then
-  . ~/.bashrc
-fi
-```
 
 And then we can add all of our goodies to `.bashrc`. And in doing so,
 everything works as planned in all scenarios between `Linux` and `MacOS`.
 
-### `.gitconfig`
+### .gitconfig
 
-Make sure to edit this file to match your preferences. Especially the below:
+Make sure to reset the user and email. Or you'll be using mine.
 
 ```bash
-[apply]
-  # Detect whitespace errors when applying a patch
-  whitespace = fix
-[core]
-  excludesfile = ~/.gitignore_global
-  # editor = atom --wait
-  # editor = code --wait
-  # editor = vi
-  editor = vim
-  # Treat spaces before tabs and all kinds of trailing whitespace as an error
-  # [default] trailing-space: looks for spaces at the end of a line
-  # [default] space-before-tab: looks for spaces before tabs at the beginning of a line
-  whitespace = space-before-tab,-indent-with-non-tab,trailing-space
-[user]
-  email = mrlesmithjr@gmail.com
-  name = Larry Smith Jr.
-[credential]
-  # Fedora
-  # helper = /usr/libexec/git-core/git-credential-gnome-keyring
-  # MacOS
-  helper = osxkeychain
-  # Ubuntu
-  # helper = cache
-  useHttpPath = true
-[color]
-  status = auto
-  branch = auto
-  interactive = auto
-  diff = auto
-[color]
-  status = auto
-  branch = auto
-  interactive = auto
-  diff = auto
-  ui = auto
-[diff]
-  tool = default-difftool
-[difftool "default-difftool"]
-  cmd = code --wait --diff $LOCAL $REMOTE
-[includeIf "gitdir:~/Git_Projects/Work/"]
-  path = ~/.gitconfig-work
-[includeIf "gitdir:~/projects/devops_automation/"]
-  path = ~/.gitconfig-work
-[status]
-  submoduleSummary = true
-[pager]
-  branch = false
-[url "git@github.com:"]
-  insteadOf = https://github.com/
-[url "git@gitlab.com:"]
-  insteadOf = https://gitlab.com/
+git config --global user.name "Your_Name"
+git config --global user.email "Your_Name@example.org"
 ```
 
-### `.vimrc` and `.vim`
+### .vimrc and .vim
 
 Some personal settings to make `vim` more pleasing on the eyes.
 
-- Install [VundleVim](https://github.com/VundleVim/Vundle.vim)
-
-```bash
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-```
-
-- Install bundles/plugins
-
-```bash
-vim ~/.vimrc
-:BundleInstall
-```
+- [VundleVim](https://github.com/VundleVim/Vundle.vim)
 
 ## Some Example Plugins/bundles and etc
 
-- [`pathogen`](https://github.com/tpope/vim-pathogen) - Manage your
+- [pathogen](https://github.com/tpope/vim-pathogen) - Manage your
   `runtimepath` with ease.
-- [`vim-ansible-yaml`](https://github.com/chase/vim-ansible-yaml) - Adds
+- [vim-ansible-yaml](https://github.com/chase/vim-ansible-yaml) - Adds
   additional syntax highlighting and fixes indentation for Ansible's dialect
   of YAML.
-- [`vim-jinja`](https://github.com/lepture/vim-jinja) - Jinja bundle for `vim`
-- [`vim-markdown`](https://github.com/plasticboy/vim-markdown) - Markdown Vim
+- [vim-jinja](https://github.com/lepture/vim-jinja) - Jinja bundle for `vim`
+- [vim-markdown](https://github.com/plasticboy/vim-markdown) - Markdown Vim
   Mode
 
 ## Visual Studio Code Extensions
@@ -167,17 +91,6 @@ you can easily import if you would like by executing the following:
 ```bash
 cat $HOME/.dotfiles/Code/extensions.list | xargs -L1 code --install-extension
 ```
-
-> NOTE: These extensions are installed automatically via Ansible provisioning when
-> following [this method](#setup-everything).
-
-## Manipulating packages to install
-
-Most all apps and etc. are provisioned via Ansible once Ansible is installed. You
-can modify packages to your liking by editing the applicable YAML files in
-`install/group_vars/all/`. Most packages will have a `state` associated with them
-so you can easily set to `present` or `absent` and Ansible will take care of the
-packages for you.
 
 ## License
 
---
+++ setup.sh
@@ -0,0 +1,343 @@
+#!/usr/bin/env bash
+set -e
+
+# Ensure $HOME/.netrc exists. Do not store this in version control
+if [ ! -f "$HOME/.netrc" ]; then
+  touch "$HOME/.netrc"
+fi
+
+# Define where dotfiles repo exists
+DOTFILES_DIR="$HOME/.dotfiles"
+
+# Define folder where to backup any existing dotfiles
+DOTFILES_DIR_BACKUP="$HOME/.dotfiles_old"
+
+# Define dotfiles to manage
+dotfiles=(".atom" ".bash_profile" ".bashrc" ".gitconfig" ".gitignore_global"
+  ".gitmessage" ".git-templates" ".hyper.js" ".pylintrc" ".rubocop.yml" ".tmux.conf" ".vim"
+  ".vimrc" ".yamllint.yml" ".zshrc")
+
+timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+
+if [ ! -d "$DOTFILES_DIR_BACKUP" ]; then
+  echo "Creating $DOTFILES_DIR_BACKUP to store any existing dotfiles..."
+  mkdir -p "$DOTFILES_DIR_BACKUP"
+  echo "Done"
+fi
+
+echo "Changing to $DOTFILES_DIR directory..."
+cd "$DOTFILES_DIR" || exit
+
+for dotfile in "${dotfiles[@]}"; do
+  if [ -f "$HOME/$dotfile" ]; then
+    if [[ ! "$HOME/$dotfile" -ef "$DOTFILES_DIR/$dotfile" ]]; then
+      echo "Backing up $dotfile to $DOTFILES_DIR_BACKUP/$dotfile.$timestamp..."
+      mv -f "$HOME/$dotfile" "$DOTFILES_DIR_BACKUP"/"$dotfile"."$timestamp"
+      echo "Done"
+    fi
+  fi
+  if [ -d "$HOME/$dotfile" ]; then
+    if [[ ! "$HOME/$dotfile" -ef "$DOTFILES_DIR/$dotfile" ]]; then
+      echo "Backing up $dotfile directory to $DOTFILES_DIR_BACKUP/$dotfile.$timestamp..."
+      mv -f "$HOME/$dotfile" "$DOTFILES_DIR_BACKUP"/"$dotfile"."$timestamp"
+      echo "Done"
+    fi
+  fi
+done
+
+cd "$HOME" || exit
+for dotfile in "${dotfiles[@]}"; do
+  if [ ! -e "$dotfile" ]; then
+    echo "Creating symlink for $DOTFILES_DIR/$dotfile..."
+    ln -s "$DOTFILES_DIR/$dotfile" .
+    echo "Done"
+  else
+    echo "$HOME/$dotfile symlink already exists..."
+  fi
+done
+
+# Check if bash-git-prompt is already installed and install if not
+if [[ $(uname) == "Linux" ]]; then
+  if [ ! -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
+    git clone https://github.com/magicmonty/bash-git-prompt.git "$HOME/.bash-git-prompt" --depth=1
+  fi
+fi
+
+# Check for existing Vundle Plugin Manager and install if missing
+# https://github.com/VundleVim/Vundle.vim
+if [ "$(ls -A "$DOTFILES_DIR/.vim/bundle/Vundle.vim/autoload")" ]; then
+  echo "Vundle already installed"
+else
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  vim +BundleInstall +qall 2 &>/dev/null
+fi
+
+if [[ $(uname) == "Darwin" ]]; then
+  # Sets up VSCode .dotfiles per https://pawelgrzybek.com/sync-vscode-settings-and-snippets-via-dotfiles-on-github/
+  VSCODE_USER_HOME="$HOME/Library/Application Support/Code/User"
+fi
+
+if [[ $(uname) == "Linux" ]]; then
+  # Sets up VSCode .dotfiles per https://pawelgrzybek.com/sync-vscode-settings-and-snippets-via-dotfiles-on-github/
+  if [ -f /etc/arch-release ]; then
+    codename="$(awk </etc/arch-release '{print $1}')"
+    if [[ $codename == "Manjaro" ]]; then
+      VSCODE_USER_HOME="$HOME/.config/Code - OSS/User"
+    fi
+  else
+    VSCODE_USER_HOME="$HOME/.config/Code/User"
+  fi
+fi
+
+if [[ ! -d $VSCODE_USER_HOME ]]; then
+  echo "Creating $VSCODE_USER_HOME ..."
+  mkdir -p "$VSCODE_USER_HOME"
+  echo "Done"
+else
+  echo "$VSCODE_USER_HOME already exists..."
+fi
+
+# Defines path to this repos .dotfiles for VSCode
+VSCODE_DOTFILES_DIR=$DOTFILES_DIR/Code
+echo "$VSCODE_DOTFILES_DIR"
+
+VSCODE_DOTFILES=("settings.json" "keybindings.json" "vsicons.settings.json")
+
+# Creates symlinks if they do not exist or creates a copy of an existing one if it exists and is not to the correct location
+for VSCODE_DOTFILE in "${VSCODE_DOTFILES[@]}"; do
+  if [[ -L "$VSCODE_USER_HOME/$VSCODE_DOTFILE" ]]; then
+    if [[ "$VSCODE_USER_HOME/$VSCODE_DOTFILE" -ef "$VSCODE_DOTFILES_DIR/$VSCODE_DOTFILE" ]]; then
+      echo "$VSCODE_USER_HOME/$VSCODE_DOTFILE symlink already correct..."
+    else
+      echo "Copying $VSCODE_USER_HOME/$VSCODE_DOTFILE $VSCODE_USER_HOME/$VSCODE_DOTFILE.$timestamp..."
+      mv "$VSCODE_USER_HOME/$VSCODE_DOTFILE" "$VSCODE_USER_HOME/$VSCODE_DOTFILE.$timestamp"
+      echo "Creating symlink for $VSCODE_USER_HOME/$VSCODE_DOTFILE..."
+      ln -s "$VSCODE_DOTFILES_DIR/$VSCODE_DOTFILE" "$VSCODE_USER_HOME/$VSCODE_DOTFILE"
+    fi
+  else
+    if [[ ! -f "$VSCODE_USER_HOME/$VSCODE_DOTFILE" ]]; then
+      echo "Creating symlink for $VSCODE_USER_HOME/$VSCODE_DOTFILE..."
+      ln -s "$VSCODE_DOTFILES_DIR/$VSCODE_DOTFILE" "$VSCODE_USER_HOME/$VSCODE_DOTFILE"
+    else
+      echo "Copying $VSCODE_USER_HOME/$VSCODE_DOTFILE $VSCODE_USER_HOME/$VSCODE_DOTFILE.$timestamp..."
+      mv "$VSCODE_USER_HOME/$VSCODE_DOTFILE" "$VSCODE_USER_HOME/$VSCODE_DOTFILE.$timestamp"
+      echo "Creating symlink for $VSCODE_USER_HOME/$VSCODE_DOTFILE..."
+      ln -s "$VSCODE_DOTFILES_DIR/$VSCODE_DOTFILE" "$VSCODE_USER_HOME/$VSCODE_DOTFILE"
+    fi
+  fi
+done
+
+if [[ -L "$VSCODE_USER_HOME/snippets" ]]; then
+  if [[ "$VSCODE_USER_HOME/snippets" -ef "$VSCODE_DOTFILES_DIR/snippets" ]]; then
+    echo "$VSCODE_USER_HOME/snippets symlink already correct..."
+  else
+    echo "Copying $VSCODE_USER_HOME/snippets $VSCODE_USER_HOME/snippets.$timestamp..."
+    mv "$VSCODE_USER_HOME/snippets" "$VSCODE_USER_HOME/snippets.$timestamp"
+    echo "Creating symlink for $VSCODE_USER_HOME/snippets..."
+    ln -s "$VSCODE_DOTFILES_DIR/snippets/" "$VSCODE_USER_HOME/snippets"
+  fi
+else
+  if [[ -d "$VSCODE_USER_HOME/snippets" ]]; then
+    echo "Copying $VSCODE_USER_HOME/snippets $VSCODE_USER_HOME/snippets.$timestamp..."
+    mv "$VSCODE_USER_HOME/snippets" "$VSCODE_USER_HOME/snippets.$timestamp"
+    echo "Creating symlink for $VSCODE_USER_HOME/snippets..."
+    ln -s "$VSCODE_DOTFILES_DIR/snippets" "$VSCODE_USER_HOME/snippets"
+  else
+    echo "Creating symlink for $VSCODE_USER_HOME/snippets..."
+    ln -s "$VSCODE_DOTFILES_DIR/snippets" "$VSCODE_USER_HOME/snippets"
+  fi
+fi
+
+# Git_Projects - Location for keeping Git projects
+# Playground - Location for throwaway projects
+# Projects - Location for keeping ongoing Projects
+
+folders=("$HOME/Git_Projects" "$HOME/Git_Projects/Personal/GitHub"
+  "$HOME/Git_Projects/Personal/GitHub/mrlesmithjr"
+  "$HOME/Git_Projects/Personal/GitLab" "$HOME/Git_Projects/Work"
+  "$HOME/Playground" "$HOME/Projects")
+
+for folder in "${folders[@]}"; do
+  if [ ! -d "$folder" ]; then
+    echo "Creating folder $folder ..."
+    mkdir -p "$folder"
+    echo "Done"
+  else
+    [ -d "$folder" ]
+    echo "Folder $folder already exists ..."
+  fi
+done
+
+# yamllint
+YAMLLINT_CONFIG="$HOME/.config/yamllint/config"
+YAMLLINT_DIR="$(dirname "$YAMLLINT_CONFIG")"
+if [ ! -d "$YAMLLINT_DIR" ]; then
+  mkdir -p "$YAMLLINT_DIR"
+fi
+if [[ -L "$YAMLLINT_CONFIG" ]]; then
+  if [[ "$YAMLLINT_CONFIG" -ef "$DOTFILES_DIR/.yamllint.yml" ]]; then
+    echo "$YAMLLINT_CONFIG symlink already correct..."
+  else
+    echo "Copying $YAMLLINT_CONFIG $YAMLLINT_CONFIG.$timestamp..."
+    mv "$YAMLLINT_CONFIG" "$YAMLLINT_CONFIG.$timestamp"
+    echo "Creating symlink for $YAMLLINT_CONFIG..."
+    ln -s "$DOTFILES_DIR/.yamllint.yml" "$YAMLLINT_CONFIG"
+  fi
+else
+  if [[ ! -f "$YAMLLINT_CONFIG" ]]; then
+    echo "Creating symlink for $YAMLLINT_CONFIG..."
+    ln -s "$DOTFILES_DIR/.yamllint.yml" "$YAMLLINT_CONFIG"
+  else
+    echo "Copying $YAMLLINT_CONFIG $YAMLLINT_CONFIG.$timestamp..."
+    mv "$YAMLLINT_CONFIG" "$YAMLLINT_CONFIG.$timestamp"
+    echo "Creating symlink for $YAMLLINT_CONFIG..."
+    ln -s "$DOTFILES_DIR/.yamllint.yml" "$YAMLLINT_CONFIG"
+  fi
+fi
+
+if [[ $(uname) == "Linux" ]]; then
+  # Arch
+  if [ -f /etc/arch-release ]; then
+    codename="$(awk </etc/arch-release '{print $1}')"
+    if [[ $codename == "Manjaro" ]]; then
+      yes | sudo pacman -Syyu && yes | sudo pacman -S gc guile autoconf automake \
+        binutils bison curl fakeroot file findutils flex gawk gcc gettext grep \
+        groff gzip libtool m4 make pacman patch pkgconf sed sudo systemd \
+        texinfo util-linux which python-setuptools python-virtualenv python-pip \
+        python-pyopenssl python2-setuptools python2-virtualenv python2-pip \
+        python2-pyopenssl
+    fi
+  fi
+
+  # Ubuntu
+  if [ -f /etc/debian_version ]; then
+    codename="$(lsb_release -c | awk '{print $2}')"
+    sudo apt-get update
+    sudo apt-get -y install build-essential curl fontconfig libffi-dev \
+      libssl-dev python-dev python-minimal python-pip python-setuptools \
+      python-virtualenv python3-pip python3-venv virtualenv
+    if [ ! -d "$HOME/.fonts" ]; then
+      mkdir "$HOME/.fonts"
+    fi
+    if [ ! -d "$HOME/.config/fontconfig" ]; then
+      mkdir -p "$HOME/.config/fontconfig/conf.d"
+    fi
+    if [ ! -f "$HOME/.fonts/PowerlineSymbols.otf" ]; then
+      wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf -O "$HOME"/.fonts/PowerlineSymbols.otf
+    fi
+    if [ ! -f "$HOME/.config/fontconfig/conf.d/10-powerline-symbols.conf" ]; then
+      fc-cache -vf "$HOME"/.fonts/
+      wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf -O "$HOME"/.config/fontconfig/conf.d/10-powerline-symbols.conf
+    fi
+  fi
+
+  # RHEL
+  if [ -f /etc/redhat-release ]; then
+    codename="$(awk </etc/redhat-release '{print $1}')"
+    if [[ $codename == "Fedora" ]]; then
+      sudo dnf -y install curl gmp-devel libffi-devel openssl-devel python-crypto \
+        python-devel python-dnf python-pip python-setuptools python-virtualenv \
+        python3-devel python3-dnf python3-setuptools python3-virtualenv \
+        redhat-rpm-config &&
+        sudo dnf -y group install "C Development Tools and Libraries"
+    elif [[ $codename == "CentOS" ]]; then
+      sudo yum -y install curl gmp-devel libffi-devel openssl-devel python-crypto \
+        python-devel python-pip python-setuptools python-virtualenv \
+        redhat-rpm-config &&
+        sudo yum -y group install "Development Tools"
+    fi
+  fi
+fi
+
+# MacOS
+if [[ $(uname) == "Darwin" ]]; then
+  if ! xcode-select --print-path &>/dev/null; then
+    xcode-select --install &>/dev/null
+  fi
+  set +e
+  command -v brew >/dev/null 2>&1
+  BREW_CHECK=$?
+  if [ $BREW_CHECK -eq 0 ]; then
+    echo "Brew already installed"
+  else
+    /usr/bin/ruby -e \
+      "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
+  brew list | grep python >/dev/null 2>&1
+  PYTHON_CHECK=$?
+  if [ $PYTHON_CHECK -eq 0 ]; then
+    echo "Python already installed"
+  else
+    brew install python
+  fi
+fi
+
+# Linux Homebrew
+if [[ $(uname) == "Linux" ]]; then
+  set +e
+  command -v brew >/dev/null 2>&1
+  BREW_CHECK=$?
+  if [ $BREW_CHECK -eq 0 ]; then
+    echo "Brew already installed"
+  else
+    bash -c \
+      "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+    test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  fi
+  if [ ! -d /home/linuxbrew/.linuxbrew/var/homebrew/linked ]; then
+    sudo mkdir -p /home/linuxbrew/.linuxbrew/var/homebrew/linked
+    sudo chown -R "$(whoami)" /home/linuxbrew/.linuxbrew/var/homebrew/linked
+  fi
+fi
+
+#### Python Virtual Environment Setup ####
+# Setup a default Python virtual environment to use rather than installing
+# everything in system
+DEFAULT_PYTHON_VERSION="3"
+VIRTUALENV_DIR="$HOME/.python-virtualenvs"
+DEFAULT_VIRTUALENV="$VIRTUALENV_DIR/default"
+PYTHON3_VIRTUALENV_DIR="$VIRTUALENV_DIR/default-python3"
+PYTHON_PIP_CMD="pip$DEFAULT_PYTHON_VERSION"
+
+# Check to ensure virtualenv command exists
+command -v virtualenv >/dev/null 2>&1
+VIRTUALENV_CMD_CHECK=$?
+if [ $VIRTUALENV_CMD_CHECK -ne 0 ]; then
+  if [[ $(uname) == "Darwin" ]]; then
+    $PYTHON_PIP_CMD install virtualenv
+  elif [[ $(uname) == "Linux" ]]; then
+    sudo $PYTHON_PIP_CMD install virtualenv
+  fi
+fi
+
+# Create Python3 default virtualenv
+if [ ! -d "$PYTHON3_VIRTUALENV_DIR" ]; then
+  if [ -f /etc/debian_version ] || [ -f /etc/redhat-release ]; then
+    python3 -m venv --system-site-packages "$PYTHON3_VIRTUALENV_DIR"
+  else
+    python3 -m venv "$PYTHON3_VIRTUALENV_DIR"
+  fi
+  # shellcheck source=/dev/null
+  source "$PYTHON3_VIRTUALENV_DIR"/bin/activate
+  $PYTHON_PIP_CMD install --upgrade pip
+  deactivate
+fi
+
+# Setup Python Virtual Environment dirs
+if [ -d "$DEFAULT_VIRTUALENV" ] && [ ! -L "$DEFAULT_VIRTUALENV" ]; then
+  mv "$DEFAULT_VIRTUALENV" "$DEFAULT_VIRTUALENV".backup
+  ln -s "$PYTHON3_VIRTUALENV_DIR" "$DEFAULT_VIRTUALENV"
+elif [ ! -d "$DEFAULT_VIRTUALENV" ]; then
+  ln -s "$PYTHON3_VIRTUALENV_DIR" "$DEFAULT_VIRTUALENV"
+elif [ -L "$DEFAULT_VIRTUALENV" ]; then
+  if [[ "$DEFAULT_VIRTUALENV" -ef "$PYTHON3_VIRTUALENV_DIR" ]]; then
+    :
+  else
+    rm "$DEFAULT_VIRTUALENV"
+    ln -s "$PYTHON3_VIRTUALENV_DIR" "$DEFAULT_VIRTUALENV"
+  fi
+fi
+
+# Source our default Python virtual environment
+# shellcheck source=/dev/null
+source "$DEFAULT_VIRTUALENV"/bin/activate
