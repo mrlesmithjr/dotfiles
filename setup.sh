6f05105b2b76d4f1d7889b559a4582dd036aabbc
Larry Smith Jr <mrlesmithjr@gmail.com>
2020-09-10 15:58:07 -0400

    Added missing packages for Ubuntu 20.04

--- setup.sh
+++ setup.sh
@@ -222,8 +222,9 @@ if [[ $(uname) == "Linux" ]]; then
 				python-setuptools python-virtualenv python3-pip python3-venv virtualenv zlib1g-dev zsh
 		else
 			sudo apt-get -y install build-essential curl fontconfig libbz2-dev libffi-dev \
-				libreadline-dev libsqlite3-dev libssl-dev python3-dev python3-minimal python3-pip \
-				python3-setuptools python3-virtualenv python3-venv virtualenv zlib1g-dev zsh
+				libreadline-dev libsqlite3-dev libssl-dev python-is-python3 python3-dev \
+				python3-minimal python3-pip python3-setuptools python3-virtualenv \
+				python3-venv virtualenv zlib1g-dev zsh
 		fi
 		if [ ! -d "$HOME/.fonts" ]; then
 			mkdir "$HOME/.fonts"
