6554ce98d070f269b6bf3cea435bf810a0e79656
Larry Smith Jr <mrlesmithjr@gmail.com>
2022-12-05 19:43:49 -0500

    Added add'l logic to install Python via pyenv

--- setup.sh
+++ setup.sh
@@ -106,10 +106,14 @@ if [ ! -d "$PYENV_ROOT" ]; then
 	git clone https://github.com/pyenv/pyenv-update.git "$PYENV_ROOT/plugins/pyenv-update"
 	git clone https://github.com/pyenv/pyenv-virtualenv.git "$PYENV_ROOT/plugins/pyenv-virtualenv"
 	export PATH="$PYENV_ROOT/bin:$PATH"
-	# DEFAULT_PYTHON_VERSION=$(pyenv install --list | grep -v - | grep -v a | grep -v b | grep -v mini | grep -v rc | tail -1 | awk '{ print $1 }')
-	# pyenv install "$DEFAULT_PYTHON_VERSION"
-	# pyenv global "$DEFAULT_PYTHON_VERSION"
-	pyenv install
+	if [ -f .python-version ]; then
+		pyenv install
+		pyenv global "$(cat .python-version)"
+	else
+		DEFAULT_PYTHON_VERSION=$(pyenv install --list | grep -v - | grep -v a | grep -v b | grep -v mini | grep -v rc | tail -1 | awk '{ print $1 }')
+		pyenv install "$DEFAULT_PYTHON_VERSION"
+		pyenv global "$DEFAULT_PYTHON_VERSION"
+	fi
 	eval "$(pyenv init --path)"
 	eval "$(pyenv init -)"
 	# eval "$(pyenv virtualenv-init -)"
