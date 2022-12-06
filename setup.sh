78d45b5366183aa728eaa1c4da83e6e2c3f75ece
Larry Smith Jr <mrlesmithjr@gmail.com>
2022-12-05 19:32:19 -0500

    Uses default Python version in repo for pyenv install

--- setup.sh
+++ setup.sh
@@ -106,9 +106,10 @@ if [ ! -d "$PYENV_ROOT" ]; then
 	git clone https://github.com/pyenv/pyenv-update.git "$PYENV_ROOT/plugins/pyenv-update"
 	git clone https://github.com/pyenv/pyenv-virtualenv.git "$PYENV_ROOT/plugins/pyenv-virtualenv"
 	export PATH="$PYENV_ROOT/bin:$PATH"
-	DEFAULT_PYTHON_VERSION=$(pyenv install --list | grep -v - | grep -v a | grep -v b | grep -v mini | grep -v rc | tail -1 | awk '{ print $1 }')
-	pyenv install "$DEFAULT_PYTHON_VERSION"
-	pyenv global "$DEFAULT_PYTHON_VERSION"
+	# DEFAULT_PYTHON_VERSION=$(pyenv install --list | grep -v - | grep -v a | grep -v b | grep -v mini | grep -v rc | tail -1 | awk '{ print $1 }')
+	# pyenv install "$DEFAULT_PYTHON_VERSION"
+	# pyenv global "$DEFAULT_PYTHON_VERSION"
+	pyenv install
 	eval "$(pyenv init --path)"
 	eval "$(pyenv init -)"
 	# eval "$(pyenv virtualenv-init -)"
